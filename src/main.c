3e6db2fe89d1d9ad8a038585ed59d22b68e15e54
muffin <diegoxe7117@gmail.com>
2021-09-29 22:31:32 +1300

    Working! Press space to gen new one

--- src/main.c
+++ src/main.c
@@ -1,39 +1,52 @@
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+// For pi
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 #include <raylib.h>
 
 #ifndef WIN_WIDTH
-#define WIN_WIDTH 400
+#define WIN_WIDTH 460
 #endif
 
 #ifndef WIN_HEIGHT
-#define WIN_HEIGHT 400
+#define WIN_HEIGHT 460
 #endif
 
-typedef
-struct xy
-{
-    int x;
-    int y;
-} xy;
+float rand_float(float min, float max);
+Vector2 *gen_slices(const Vector2 centre, const float r, const int num_slices);
+Vector2 rand_pt_circle(const Vector2 centre, const float r);
 
 int
 main(int argc, char **argv)
 {
-    /* def circle params
-     *      radius
-     *      xy of center
-     *
-     * def num slices. Evenly distribute through circumference. (2pi)/num slices.
-     *
-     * Get a random value 0 <= x <= 2pi, get a random value 0 < x < radius. Polar coords?
-     *
-     * Connect edges of the slices to the random centre.
-     */
+    // TO DO: Add pizza drawing
+    // EXTENSION: oval pizza
 
-    const unsigned int r = 40;
-    const xy centre = { 100, 100 };
+    srand((unsigned)time(NULL));
+
+
+    // Get num slices from cli arg
+    int _slices;
+
+    if (argc > 1)
+        _slices = atoi(argv[1]);
+    else
+        _slices = 8;
+
+    const int num_slices = _slices;
+
+    const float r = (fminf(WIN_HEIGHT, WIN_HEIGHT) / 2)-20;
+    const Vector2 centre = { WIN_WIDTH / 2, WIN_HEIGHT / 2 };
+
+    // Generate slices at circumference
+    Vector2 *slices = gen_slices(centre, r, num_slices);
+
+    // Get a new center
+    Vector2 bad_centre = rand_pt_circle(centre, r);
 
     InitWindow(WIN_WIDTH, WIN_HEIGHT, "Why");
 
@@ -41,15 +54,64 @@ main(int argc, char **argv)
     {
         BeginDrawing();
             ClearBackground(WHITE);
+            DrawCircleLines(centre.x, centre.y, r, BLACK);
 
-            // Draw
+            for (int i = 0; i < num_slices; i++)
+                DrawLineV(slices[i], bad_centre, BLACK);
+
         EndDrawing();
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            // Create new pizza
+            free(slices);
+            slices = gen_slices(centre, r, num_slices);
+            bad_centre = rand_pt_circle(centre, r);
+        }
     }
-
     CloseWindow();
 
-    // TO DO: Add pizza drawing
-    // EXTENSION: oval pizza
+    free(slices);
 
     return 0;
+}
+
+float rand_float(const float min, const float max)
+{
+    return min + ((double)rand() / RAND_MAX) * (max - min);
+
+}
+
+Vector2 *gen_slices(const Vector2 centre, const float r, const int num_slices)
+{
+    Vector2 *slices = calloc(sizeof(Vector2), num_slices);
+
+    // The diff in radians between each slice.
+    const double offset = (2 * M_PI) / num_slices;
+
+    for (int i = 0; i < num_slices; i++)
+    {
+        float uneven = rand_float(0, M_PI/8);
+        slices[i] = (Vector2){
+            centre.x + r * cos(offset * i + uneven),
+            centre.y + r * sin(offset * i + uneven)
+        };
+    }
+
+    return slices;
+}
+
+Vector2 rand_pt_circle(const Vector2 centre, const float r)
+{
+    /* Generates a random point inside a circle */
+    const float rad = rand_float(0, M_PI*2);
+
+    const float x = rand_float(0, 1);
+    /* const float dist = r * 6 * (pow(x - 1, 3) + pow(x - 1, 2)) + 0.05; */
+    /* const float dist = r * ((pow(x - 0.5, 2) * 3) / 4 + 0.25); */
+    const float dist = r * (pow(x - 0.55, 2) / 4 + 0.1);
+
+    return (Vector2){
+            centre.x + dist * cos(rad),
+            centre.y + dist * sin(rad)
+    };
 }
